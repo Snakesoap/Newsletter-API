f0319cec2f462b840a11c5a0bb8f43f58241a3d5
Princeton <56801842+Snakesoap@users.noreply.github.com>
2020-01-16 11:05:43 -0600

    Update app.js
    Added port function

--- app.js
+++ app.js
@@ -68,7 +68,12 @@ app.post("/failure.html", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Its up on 3000");
 
 });
